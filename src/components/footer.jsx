7a7f984227c6ba2e6e3e21f9a00eaccad05795b8
jamiltarek55 <77003619+jamiltarek55@users.noreply.github.com>
2022-11-13 09:13:36 +0600

    Add files via upload

---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Footer extends React.Component{
+
+  render(){
+    return(
+      <footer className='footer'>
+        POKEMON_API - 2022 <br/>
+        Develop With ❤️️ By Tarek Jamin
+      </footer>
+    )
+  }
+}
